9b04379bc8fbc618d7c0675d624a37fe6c57cbbe
DanielGecheski <119902873+DanielGecheski@users.noreply.github.com>
2023-03-19 21:08:00 +0100

    Add files via upload

---
+++ Class02_Task01/Class02_Task01/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Class2_Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 1");
+
+            Console.WriteLine("Please enter the first number:");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number:");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What operation do you want to do: +, -, * or /):");
+            string operation = Console.ReadLine();
+
+            int result;
+            switch (operation)
+            {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    result = firstNumber / secondNumber;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+            Console.WriteLine($"The result is: {result}");
+        }
+    }
+}
---
+++ Class02_Task02/Class02_Task02/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Class03_Task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 2");
+
+            Console.Write("Enter the First number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter the Second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter the third number: ");
+            int thirdNumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter the four number: ");
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            int averageNumber = (firstNumber + secondNumber + thirdNumber + fourthNumber) / 4;
+
+            Console.WriteLine("The average of {0}, {1}, {2} and {3} is: {4}", firstNumber, secondNumber, thirdNumber, fourthNumber, averageNumber);
+        }
+    }
+}
