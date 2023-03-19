d61b7df50fcfd433e0b3da13a8cc6d8fdae00adc
DanielGecheski <119902873+DanielGecheski@users.noreply.github.com>
2023-03-19 21:15:16 +0100

    Add files via upload

---
+++ Class03_Task01/Class03_Task01/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Class03_Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 1");
+
+            int [] numbers = new int[6];
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Enter integer no.{0}: ", i + 1);
+                numbers[i] = int.Parse(Console.ReadLine());
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+            Console.WriteLine("The result is: {0}", sum);
+        }
+    }
+}
---
+++ Class03_Task01/Class03_Task02/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Class03_Task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 2");
+
+            string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsG2 = { "Daniel", "Risto", "Tijana", "Ana", "Marija" };
+
+            Console.WriteLine("Enter student group (1 or 2):");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                Console.WriteLine("The Students in G1 are:");
+                foreach (string student in studentsG1)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+            else if (input == "2")
+            {
+                Console.WriteLine("The Students in G2 are:");
+                foreach (string student in studentsG2)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
+    }
+}
