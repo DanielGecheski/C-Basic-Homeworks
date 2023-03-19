1b376dc24d15dab5967dc663e4a7964bec64be5a
DanielGecheski <119902873+DanielGecheski@users.noreply.github.com>
2023-03-19 21:09:20 +0100

    Add files via upload

---
+++ Class02_Task03/Class02_Task03/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Class03_Task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 3");
+
+            int firstNumber, secondNumber, swapNumbers;
+
+            Console.Write("Input the First Number: ");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the Second Number: ");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            swapNumbers = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = swapNumbers;
+
+            Console.WriteLine("Swapped numbers:");
+            Console.WriteLine("First Number: {0}", firstNumber);
+            Console.WriteLine("Second Number: {0}", secondNumber);
+        }
+    }
+}
---
+++ Class03_Exercise06/Class03_Exercise06/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Class03_Exercise06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercise 6");
+
+            Console.WriteLine("Please enter two numbers:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            
+
+            if (firstNumber == secondNumber)
+            {
+                Console.Write("You have entered the same number! They are equal!");
+                return;
+            }
+            
+
+            int largerNumber;
+
+            if (firstNumber > secondNumber)
+            {
+                largerNumber = firstNumber;
+            }
+            else
+            {
+                largerNumber = secondNumber;
+            }
+
+    
+            string evenOrOdd;
+
+            if (largerNumber % 2 == 0)
+            {
+                evenOrOdd = "even";
+            }
+            else
+            {
+                evenOrOdd = "odd";
+            }
+
+            Console.WriteLine($"The larger number is number {largerNumber} and it is {evenOrOdd}.");
+
+         
+        }
+    }
+}
